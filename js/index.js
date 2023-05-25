3f868ca3231f8b5701dc65f502fc987d41691d0e
AshwinKumar-V <ashwingt20001@gmail.com>
2023-05-25 11:31:41 +0530

    Seperated folder structure for css and js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function fullName() {
+    var firstName = document.getElementById("firstName").value
+    var lastName = document.getElementById("lastName").value
+    alert("Full name: " + firstName + " " + lastName)
+    return
+}
