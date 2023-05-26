c77d747deb2382fb7d3ec3f3b0587ae75bafb393
AshwinKumar-V <ashwingt20001@gmail.com>
2023-05-26 10:52:01 +0530

    Added onClick to Movies Card

--- ReactProject-1/src/components/Content.tsx
+++ ReactProject-1/src/components/Content.tsx
@@ -1,11 +1,10 @@
 function Content() {
-  const content = "Continue watching..."
+  const default_msg = "No new movies."
   const movies = ['Inception', 'Interstellar', 'Tenet', 'Oppenheimer']
 
   return (
     <div className="content">
-      <h4>{content}</h4>
-
+      {movies.length == 0 ? <p>{default_msg}</p> : null}
       <div className="flex_row flex-wrap">
         {movies.map((movie) => (
           <div className="card m-2" key={movie}>
@@ -13,13 +12,13 @@ function Content() {
             <div className="card-body">
               <h5 className="card-title">{movie}</h5>
               <p className="card-text">Some random movie description.</p>
-              <a href="#" className="btn btn-primary">Watch Now</a>
+              <a href="#" className="btn btn-primary" onClick={() => console.log("Watching " + movie)}>Watch Now</a>
             </div>
           </div>
         ))}
       </div>
 
-    </div>
+    </div >
   )
 }
 
