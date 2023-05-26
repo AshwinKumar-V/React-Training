d03a6c3538da2d97425d506911c3ead50e3300d4
AshwinKumar-V <ashwingt20001@gmail.com>
2023-05-26 10:29:31 +0530

    List map for movies added

--- ReactProject-1/src/components/Content.tsx
+++ ReactProject-1/src/components/Content.tsx
@@ -1,19 +1,26 @@
 function Content() {
-    const content = "Continue watching..."
+  const content = "Continue watching..."
+  const movies = ['Inception', 'Interstellar', 'Tenet', 'Oppenheimer']
 
-    return (
-        <div className="content">
-            <p>{content}</p>
-            <div className="card">
-              {/* <img src="..." className="card-img-top" alt="..."> */}
-              <div className="card-body">
-                <h5 className="card-title">Random Movie</h5>
-                <p className="card-text">Some random movie description.</p>
-                <a href="#" className="btn btn-primary">Watch Now</a>
-              </div>
+  return (
+    <div className="content">
+      <h4>{content}</h4>
+
+      <div className="flex_row flex-wrap">
+        {movies.map((movie) => (
+          <div className="card m-2" key={movie}>
+            {/* <img src="..." className="card-img-top" alt="..."> */}
+            <div className="card-body">
+              <h5 className="card-title">{movie}</h5>
+              <p className="card-text">Some random movie description.</p>
+              <a href="#" className="btn btn-primary">Watch Now</a>
             </div>
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+
+    </div>
+  )
 }
 
 export default Content;
