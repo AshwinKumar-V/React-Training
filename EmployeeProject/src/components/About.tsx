f46fba401838766576a7004d7126c8dcbbd340b7
AshwinKumar-V <ashwingt20001@gmail.com>
2023-05-31 10:07:25 +0530

    Custom hook (useRandomData) added

--- EmployeeProject/src/components/About.tsx
+++ EmployeeProject/src/components/About.tsx
@@ -1,28 +1,16 @@
 import axios from "axios"
 import { useEffect, useRef, useState } from "react"
+import useRandomData from "../hooks/useRandomData"
 
 export const About = () => {
 
-    const [data, updateData] = useState(0)
-    const [error, updateError] = useState(null)
-
-    // to call api and update view
-    const ref = useRef({})
-    useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/todos/" + data)
-            .then((res) => {
-                ref.current = res.data
-                updateError(null)
-            })
-            .catch((err) => updateError(err.message))
-    })
+    const { data, error, updateData, ref, isLoading } = useRandomData()
 
     return (
         <>
             <h1>About</h1>
             <button onClick={() => updateData(data + 1)}>Change</button>
-            {Object.keys(ref.current).map((x) => (<h4 key={x}>{x} : {ref.current[x]}</h4>))}
-            <p>{error}</p>
+            {isLoading ? <h4>Loading...</h4> : (error ? <p>{error}</p> : Object.keys(ref.current).map((x) => (<h4 key={x}>{x} : {ref.current[x]}</h4>)))}
         </>
 
     )
---
+++ EmployeeProject/src/hooks/useRandomData.ts
@@ -0,0 +1,27 @@
+import axios from "axios"
+import { useEffect, useRef, useState } from "react"
+
+
+const useRandomData = () => {
+
+    const [data, updateData] = useState(0)
+    const [error, updateError] = useState(null)
+    const [isLoading, updateLoading] = useState(false)
+
+    // to call api and update view
+    const ref = useRef({})
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/todos/" + data)
+            .then((res) => {
+                ref.current = res.data
+                updateError(null)
+            })
+            .catch((err) => updateError(err.message))
+            .finally(() => updateLoading(false))
+        updateLoading(true)
+    }, [data])
+
+    return ({ data, error, updateData, ref, isLoading })
+}
+
+export default useRandomData
