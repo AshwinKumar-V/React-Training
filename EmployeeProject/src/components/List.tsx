d0f65e0125a16782464fc569c18d71e54e3cee98
AshwinKumar-V <ashwingt20001@gmail.com>
2023-05-31 17:35:29 +0530

    Bug: user data not rendering in List

--- EmployeeProject/src/components/List.tsx
+++ EmployeeProject/src/components/List.tsx
@@ -1,6 +1,8 @@
 import { Link, useNavigate } from 'react-router-dom'
 import clients from '../assets/clients'
-import users from '../assets/users'
+import useUsers from '../hooks/useUsers'
+import { useEffect } from 'react'
+// import users from '../assets/users'
 
 interface Props {
     listType: string
@@ -9,15 +11,20 @@ interface Props {
 const List = ({ listType }: Props) => {
 
     const navigate = useNavigate()
+    const { data, error, isLoading } = useUsers()
 
     // check if client-list or user-list is loaded
     let list = null
-    if (listType === 'Client') {
-        list = clients
-    }
-    else if (listType === 'User') {
-        list = users
-    }
+    useEffect(() => {
+        if (listType === 'Client') {
+            list = clients
+        }
+        else if (listType === 'User' && data) {
+            list = data
+        }
+    }, [data])
+
+
 
     // redirect to client/user details page
     const showDetails = (item: any) => {
@@ -28,23 +35,25 @@ const List = ({ listType }: Props) => {
         <>
             <h1>{listType} list</h1>
             {listType === 'User' ? <button className='btn'><Link to='add-user'>Add User</Link></button> : null}
-            <table className='table'>
-                <thead>
-                    <tr>
-                        {Object.keys(list[0]).map((x, i) => (
-                            <th key={i}>{x}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {list?.map((x) => (
-                        <tr key={Object.values(x)[0]}>
-                            {Object.values(x).map((data, i) => (<td key={i}>{data}</td>))}
-                            <td onClick={() => showDetails(x)}><u className='details'>Details</u></td>
+
+            {isLoading ? <h4>Loading...</h4> : (error ? <p>{error}</p> :
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            {Object.keys(list[0]).map((x, i) => (
+                                <th key={i}>{x}</th>
+                            ))}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {list?.map((x) => (
+                            <tr key={Object.values(x)[0]}>
+                                {Object.values(x).map((data, i) => (<td key={i}>{data}</td>))}
+                                <td onClick={() => showDetails(x)}><u className='details'>Details</u></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>)}
         </>
     )
 }
